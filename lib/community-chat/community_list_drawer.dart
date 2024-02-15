a8e21d23221d038d47f5bbac5dde41b7cca1ff59
Hxrsha-x <ahkharsha@gmail.com>
2024-02-15 09:56:03 +0530

    add 3 default communities

--- lib/community-chat/community_list_drawer.dart
+++ lib/community-chat/community_list_drawer.dart
@@ -7,7 +7,7 @@ import 'package:pregathi/const/constants.dart';
 import 'package:pregathi/community-chat/screens/create_community_screen.dart';
 
 class CommunityDrawer extends ConsumerWidget {
-  const CommunityDrawer({super.key});
+  const CommunityDrawer({Key? key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -22,59 +22,96 @@ class CommunityDrawer extends ConsumerWidget {
                 goTo(context, CreateCommunityScreen());
               },
             ),
-            FutureBuilder(
-              future:
-                  FirebaseFirestore.instance.collection('communities').get(),
-              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('communities')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (snapshot.hasError) {
                   return dialogueBox(
                       context, 'Some error has occurred ${snapshot.error}');
                 }
 
-                if (snapshot.hasData) {
-                  QuerySnapshot querySnapshot = snapshot.data;
-                  List<QueryDocumentSnapshot> documents = querySnapshot.docs;
-
-                  List<Map<String, dynamic>> items = documents
-                      .map(
-                        (community) => {
-                          'members': community['members'],
-                          'name':community['name'],
-                          'avatar':community['avatar'],
-                        },
-                      )
-                      .toList();
-
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final String userUid =
-                            FirebaseAuth.instance.currentUser!.uid;
-                        Map<String, dynamic> thisItem = items[index];
-                        print(thisItem['members']);
-                    
-                        if (thisItem['members'].contains(userUid)) {
-                          return ListTile(
-                            leading: CircleAvatar(
-                              backgroundImage: NetworkImage(thisItem['avatar']),
-                            ),
-                            title: Text('${thisItem['name']}'),
-                            onTap: () {
-                              goTo(context,
-                                  CommunityScreen(name: '${thisItem['name']}'));
-                            },
-                       
-                          );
-                        }
-                    
-                        return Container();
-                      },
-                    ),
-                  );
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return progressIndicator(context);
                 }
 
-                return progressIndicator(context);
+                List<Map<String, dynamic>> items = snapshot.data!.docs
+                    .map(
+                      (community) => {
+                        'members': community['members'],
+                        'name': community['name'],
+                        'avatar': community['avatar'],
+                      },
+                    )
+                    .toList();
+
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final String userUid =
+                          FirebaseAuth.instance.currentUser!.uid;
+                      Map<String, dynamic> thisItem = items[index];
+                      print(thisItem['members']);
+
+                      if (thisItem['name'] == '1st Trimester') {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(thisItem['avatar']),
+                          ),
+                          title: Text('${thisItem['name']}'),
+                          onTap: () {
+                            goTo(context,
+                                CommunityScreen(name: '${thisItem['name']}'));
+                          },
+                        );
+                      }
+
+                      if (thisItem['name'] == '2nd Trimester') {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(thisItem['avatar']),
+                          ),
+                          title: Text('${thisItem['name']}'),
+                          onTap: () {
+                            goTo(context,
+                                CommunityScreen(name: '${thisItem['name']}'));
+                          },
+                        );
+                      }
+
+                      if (thisItem['name'] == '3rd Trimester') {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(thisItem['avatar']),
+                          ),
+                          title: Text('${thisItem['name']}'),
+                          onTap: () {
+                            goTo(context,
+                                CommunityScreen(name: '${thisItem['name']}'));
+                          },
+                        );
+                      }
+
+                      if (thisItem['members'].contains(userUid)) {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(thisItem['avatar']),
+                          ),
+                          title: Text('${thisItem['name']}'),
+                          onTap: () {
+                            goTo(context,
+                                CommunityScreen(name: '${thisItem['name']}'));
+                          },
+                        );
+                      }
+
+                      return Container();
+                    },
+                  ),
+                );
               },
             ),
           ],
