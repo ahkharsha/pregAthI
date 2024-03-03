290aaf691f8399e458969042e56e6111a4b84a14
N-SARAN <n.sarancs@gmail.com>
2024-03-04 00:31:23 +0530

    videoPlayer-added

--- lib/main-screens/about_us.dart
+++ lib/main-screens/about_us.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import 'package:pregathi/const/constants.dart';
 
-class AboutUsScreen extends StatelessWidget {
+class AboutUsScreen extends StatefulWidget {
   AboutUsScreen({Key? key}) : super(key: key);
+
+  @override
+  _AboutUsScreenState createState() => _AboutUsScreenState();
+}
+
+class _AboutUsScreenState extends State<AboutUsScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+  bool _showControls = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
+    );
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Start playing the video when the page is visible
+    _controller.play();
+    _showControls = false; // Hide controls initially
+  }
+
+  void _toggleControlsVisibility() {
+    setState(() {
+      _showControls = !_showControls;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +66,72 @@ class AboutUsScreen extends StatelessWidget {
         centerTitle: false,
         backgroundColor: primaryColor,
       ),
-      body: Center(
-        child: Text('This is the about us page'),
+      body: GestureDetector(
+        onTap: _toggleControlsVisibility,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _buildVideoPlayer(),
+            if (_showControls)
+              Positioned(
+                bottom: 16.0,
+                child: _buildVideoProgressBar(),
+              ),
+            if (_showControls)
+              Positioned(
+                bottom: 80.0,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_controller.value.isPlaying) {
+                        _controller.pause();
+                      } else {
+                        _controller.play();
+                      }
+                    });
+                  },
+                  child: Icon(
+                    _controller.value.isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildVideoPlayer() {
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildVideoProgressBar() {
+    return Container(
+      width: 300.0, // Adjust the width as needed
+      child: VideoProgressIndicator(
+        _controller,
+        allowScrubbing: true,
+        colors: VideoProgressColors(
+          playedColor: Color.fromARGB(255, 18, 116, 228),
+          bufferedColor: Colors.grey,
+          backgroundColor: Colors.black,
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,11 +15,13 @@ import firebase_storage
 import flutter_local_notifications
 import geolocator_apple
 import google_sign_in_ios
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import video_player_avfoundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -32,9 +34,11 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
